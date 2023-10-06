d3fec4e26e5b89bb24461c0c61d5b355b56ca78f
zaytsev <zaicev@mniti.ru>
2023-10-06 16:32:17 +0300

    Added new file

---
+++ Course/06/06.2-script.js
@@ -0,0 +1,14 @@
+const app_062 = angular.module('app', [])
+
+app_062.controller("emp_06", function ($scope) {
+    $scope.a = 3
+    $scope.b = 7
+
+    $scope.doDoubleAndSum = function () {
+
+        $scope.a = $scope.a * 2
+        $scope.b = $scope.b * 2
+        $scope.sum = parseInt($scope.a) + parseInt($scope.b)
+
+    }
+})
